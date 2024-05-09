b7249290ab1e62c2f4a91cd25b6f66531492ca0b
shifanseta <shifanseta@gmail.com>
2024-05-09 15:07:29 +0530

    build error resolved

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -10,11 +10,14 @@ import Loader from '@/components/ui/Loader';
 
 const MeetingIDPage = () => {
   let params = useParams();
+  console.log("params", params);
+  
   let paramsId:any = params.id
   const [isSetupComplete, setIsSetupComplete] = useState(false)
   const {call, isCallLoading} = useGetCallbyId(paramsId);
   const {user, isLoaded} = useUser();
-
+  console.log("paramsId", paramsId);
+  
   console.log(call);
   
 
